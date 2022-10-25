1996a939c8a37b2fa8eb86a79de1184ebd83b934
Will Minshew <wminshew@gmail.com>
2022-10-24 20:42:40 -0700

    update script

--- script/LiquidSplitFactory.s.sol
+++ script/LiquidSplitFactory.s.sol
@@ -9,8 +9,8 @@ contract LiquidSplitFactoryScript is Script {
         uint256 privKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(privKey);
 
-        /* new LiquidSplitFactory{salt: keccak256("0xSplits.liquid.v1")}(0x2ed6c4B5dA6378c7897AC67Ba9e43102Feb694EE); */
-        new LiquidSplitFactory(0x2ed6c4B5dA6378c7897AC67Ba9e43102Feb694EE);
+        new LiquidSplitFactory{salt: keccak256("0xSplits.liquid.v1")}(0x2ed6c4B5dA6378c7897AC67Ba9e43102Feb694EE);
+        /* new LiquidSplitFactory(0x2ed6c4B5dA6378c7897AC67Ba9e43102Feb694EE); */
 
         vm.stopBroadcast();
     }
