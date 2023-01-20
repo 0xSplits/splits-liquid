5e97c3ff00495d87365d63127c85a6ac3ceca2f0
Will Minshew <wminshew@gmail.com>
2023-01-20 15:51:38 -0800

    deploy to bsc, testnet

--- script/LiquidSplitFactory.s.sol
+++ script/LiquidSplitFactory.s.sol
@@ -9,8 +9,8 @@ contract LiquidSplitFactoryScript is Script {
         uint256 privKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(privKey);
 
-        new LiquidSplitFactory{salt: keccak256("0xSplits.liquid.v1")}(0x2ed6c4B5dA6378c7897AC67Ba9e43102Feb694EE);
-        /* new LiquidSplitFactory(0x2ed6c4B5dA6378c7897AC67Ba9e43102Feb694EE); */
+        /* new LiquidSplitFactory{salt: keccak256("0xSplits.liquid.v1")}(0x2ed6c4B5dA6378c7897AC67Ba9e43102Feb694EE); */
+        new LiquidSplitFactory{salt: keccak256("0xSplits.bsc.liquid.v1")}(0x5924cD81dC672151527B1E4b5Ef57B69cBD07Eda);
 
         vm.stopBroadcast();
     }
